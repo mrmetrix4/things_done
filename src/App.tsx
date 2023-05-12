e3b6254a1d257d8a6dcf11ad2bba3601dc4783ee
Adar Arbiv <mrmetrix4@gmail.com>
2023-05-12 18:16:47 +0300

    Added fontAwsome.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
-import { appWindow } from "@tauri-apps/api/window";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
     return (
         <div className="container">
             <h1>Things to do</h1>
+            <FontAwesomeIcon icon={faBars} />
             <footer>
                 <p>Designed and developed by Adar Arbiv</p>
             </footer>
