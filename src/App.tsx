ba913b2d38aba7927587f83dd6090f2eb3573b20
Adar Arbiv <mrmetrix4@gmail.com>
2023-05-16 21:02:35 +0300

    Added menu!

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faL } from "@fortawesome/free-solid-svg-icons";
 import TaskList from "./Tasks/TaskList";
+import { open } from "@tauri-apps/api/shell";
+import { useState } from "react";
 
 function App() {
+    const [menuLinks, setMenuLinks] = useState(false);
+    async function handleMenuClick() {
+        console.log("Clicked");
+        setMenuLinks((prevState) => !prevState);
+    }
+
     return (
-        <div className="container">
-            <h1>Things to do</h1>
-            <FontAwesomeIcon icon={faBars} />
-            <TaskList />
-            <footer>
-                <p>Designed and developed by Adar Arbiv</p>
-            </footer>
-        </div>
+        <>
+            <nav role="navigation" className="main-nav" id="main-nav">
+                <ul id="main-nav-list">
+                    <h1>Things to do</h1>
+                    <li>
+                        <div className="icon">
+                            <div
+                                className="itemys"
+                                style={{
+                                    display: menuLinks ? "flex" : "none",
+                                }}
+                            >
+                                <button onClick={() => console.log("clickMu")}>
+                                    click me
+                                </button>
+                                <button onClick={() => console.log("clickMe")}>
+                                    click mu
+                                </button>
+                            </div>
+                            <svg
+                                onClick={handleMenuClick}
+                                viewBox="0 0 100 100"
+                                className="icon"
+                            >
+                                <FontAwesomeIcon icon={faBars} />
+                            </svg>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+            <div className="container">
+                <TaskList />
+                <footer>
+                    <p>Designed and developed by Adar Arbiv</p>
+                </footer>
+            </div>
+        </>
     );
 }
 
