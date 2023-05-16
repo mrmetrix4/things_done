103bfe25765934d7b16c3600a092f91cc1a5a1fa
Adar Arbiv <mrmetrix4@gmail.com>
2023-05-16 18:59:43 +0300

    Added checkbox.

--- src/Tasks/TaskList.tsx
+++ src/Tasks/TaskList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { dumpTasks, loadTasks } from "./tasksIo";
 import Task from "./Task";
+import { event } from "@tauri-apps/api";
 
 function TaskList() {
     const [tasks, setTasks] = useState<Task[]>([]);
@@ -16,10 +17,25 @@ function TaskList() {
         setTasks((prevTasks) => [...prevTasks, nTask]);
     }
 
+    async function handleOnChange(e: any) {
+        const { checked, id } = e.target;
+        console.log(id);
+        setTasks((prevTasks) =>
+            prevTasks.map((t) => (t.id == id ? { ...t, done: checked } : t))
+        );
+    }
+
     return (
         <div>
             {tasks.map((t, index) => (
                 <p key={index}>
+                    <input
+                        type="checkbox"
+                        checked={t.done}
+                        onChange={handleOnChange}
+                        name="task_checkbox"
+                        id={t.id}
+                    />
                     {t.title}:{t.description}
                 </p>
             ))}
