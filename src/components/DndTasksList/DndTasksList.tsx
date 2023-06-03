59b321cb5305ceb43047148b7aef5d793f4ea614
Adar Arbiv <mrmetrix4@gmail.com>
2023-06-03 21:44:58 +0300

    Fixed classes.

--- src/components/DndTasksList/DndTasksList.tsx
+++ src/components/DndTasksList/DndTasksList.tsx
@@ -15,42 +15,43 @@ function DndTasksList(props: IDndTasksListProps) {
         ? tasks.filter((task) => task.parentTaskID === props.parentTaskID)
         : tasks.filter((task) => !task.parentTaskID);
     return (
-        <DragDropContext
-            onDragEnd={(result: DropResult) => {
-                dispatchTasks({ type: "reorder", result: result });
-            }}
-        >
-            <StrictModeDroppable droppableId="characters">
-                {(provided) => (
-                    <ul
-                        className="characters"
-                        {...provided.droppableProps}
-                        ref={provided.innerRef}
-                    >
-                        {filteredTasks.map((task, index) => {
-                            return (
-                                <Draggable
-                                    key={task.id}
-                                    draggableId={task.id}
-                                    index={index}
-                                >
-                                    {(provided) => (
-                                        <div
-                                            ref={provided.innerRef}
-                                            {...provided.draggableProps}
-                                            {...provided.dragHandleProps}
-                                        >
-                                            <Task task={task} />
-                                        </div>
-                                    )}
-                                </Draggable>
-                            );
-                        })}
-                        {provided.placeholder}
-                    </ul>
-                )}
-            </StrictModeDroppable>
-        </DragDropContext>
+        <div className="tasklist">
+            <DragDropContext
+                onDragEnd={(result: DropResult) => {
+                    dispatchTasks({ type: "reorder", result: result });
+                }}
+            >
+                <StrictModeDroppable droppableId="tasklist">
+                    {(provided) => (
+                        <ul
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                        >
+                            {filteredTasks.map((task, index) => {
+                                return (
+                                    <Draggable
+                                        key={task.id}
+                                        draggableId={task.id}
+                                        index={index}
+                                    >
+                                        {(provided) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                            >
+                                                <Task task={task} />
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                );
+                            })}
+                            {provided.placeholder}
+                        </ul>
+                    )}
+                </StrictModeDroppable>
+            </DragDropContext>
+        </div>
     );
 }
 
